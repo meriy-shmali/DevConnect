7e151318d37a3790b5df6d90db5d2dedf7077582
meriy-shmali <meriyshmali@gmail.com>
2025-11-25 16:26:28 +0200

    add buttons

--- src/components/ui/ButtonGroup.jsx
+++ src/components/ui/ButtonGroup.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Button } from './button'
 import { FaArrowRightLong } from "react-icons/fa6";
 import { MdOutlineGTranslate } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { FiLogOut } from "react-icons/fi";
+import { FaRobot } from "react-icons/fa";
 const Buttons = ({ type }) => {
   if (type === "start now") {
     return (
@@ -26,6 +29,64 @@ const Buttons = ({ type }) => {
         Translate
       </Button>
     );
+  }
+  if(type=='follow'){
+    return(
+      <Button variant='secondary'
+      className='bg-follow-button text-text-button text-[24px] hover:bg-hover-purple'
+      size='sm'>Follow</Button>
+    )
+  }
+   if(type=='edit'){
+    return(
+      <Button variant='secondary'
+      className='bg-follow-button text-text-button text-[24px] hover:bg-hover-purple'
+      size='sm'>Edit<MdEdit className='size-[22px]' /></Button>
+    )
+  }
+  if(type=='cancel'){
+    return(
+      <Button variant='default'
+      className='bg-cancel-button text-text-button text-[24px]'
+      size='default'>
+Cancel
+      </Button>
+    )
+  }
+  if(type=='logout'){
+    return(
+      <Button className='bg-cancel-button text-text-button text-[24px]' size='lg'>Logout <FiLogOut className='size-[22px]' /></Button>
+    )
+  }
+  if(type=='login'){
+    return(
+  <Button className='bg-blue-button text-text-button md:text-[25px]' variant='default' size='lg'>
+    Login
+  </Button>)}
+  if(type=='send'){
+    return(
+      <Button className='bg-blue-button text-text-button md:text-[25px]' variant='default'>Send</Button>
+    )
+  }
+ if(type=='post'){
+    return(
+      <Button className='bg-blue-button text-text-button md:text-[25px]' variant='default'>Post</Button>
+    )
+  }
+   if(type=='use'){
+    return(
+      <Button className='bg-blue-button text-text-button md:text-[25px]' variant='default'>Use</Button>
+    )
+  }
+   if(type=='save'){
+    return(
+      <Button className='bg-blue-button text-text-button md:text-[25px]' variant='default'>Save</Button>
+    )
+  }
+  if(type=='ai'){
+    return(
+      <Button className='border-ai-assistant text-ai-assistant bg-white text-[22px]' variant='outline'><FaRobot className='text-ai-assistant size-[28px]' size='lg'/>AI Assistant </Button>
+    )
   }
 
   return null;
